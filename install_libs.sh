d87a980a3d9b62a50b80b01b030e9c314a8c4b56
Rafael <rafael.pierre@gmail.com>
2019-03-22 10:50:11 +0100

    kafka

---
+++ install_libs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -xe
+
+# Non-standard and non-Amazon Machine Image Python modules:
+sudo pip install -U \
+  mne            \
+  scipy	       \
+  itertools   \
+  pandas      \
+  numpy
